da407dce7279c3cab09dfccba51a7e076af6201b
MisayaQ <516802581@qq.com>
2021-03-25 15:52:11 +0800

    配置文件的改写

--- src/test/java/com/misaya/ShardingJdbcApplicationTests.java
+++ src/test/java/com/misaya/ShardingJdbcApplicationTests.java
@@ -1,13 +1,29 @@
 package com.misaya;
 
+import com.misaya.entity.User;
+import com.misaya.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
+import java.util.Random;
 
 @SpringBootTest
 class ShardingJdbcApplicationTests {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @Test
     void contextLoads() {
+        User user = new User();
+        user.setNickname("zhangsan"+ new Random().nextInt());
+        user.setPassword("1234567");
+        user.setSex(1);
+        user.setBirthday(new Date());
+        user.setAge(1);
+        userMapper.addUser(user);
     }
 
 }
